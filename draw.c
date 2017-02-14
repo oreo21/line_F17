f52c2a0fc1251f9d51a81a206c750419cee19ac5
oreo21 <reok99@gmail.com>
2017-02-14 10:59:38 -0500

    implemented drawing for octant I -- not tested

--- draw.c
+++ draw.c
@@ -7,5 +7,17 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  float slope = (y1 - y0)/(x1 - x0);
+  int A = y1 - y0, B = x0 - x1;
+  int x = x0, y = y0;
+  int d = 2 * A + B;
+  while(x <= x1) {
+    plot(s, c, x, y);
+    if(d > 0) {
+      y++;
+      d += 2 * B;
+    }
+    x++;
+    d += 2 * A;
+  }
 }
-
